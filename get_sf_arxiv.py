17bbf2bbd0d55114dd9e13b363e65b59fdbdf52b
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2020-06-29 16:56:01 -0400

    py3 syntax

--- get_sf_arxiv.py
+++ get_sf_arxiv.py
@@ -2,7 +2,7 @@ import arxiv, datetime, json
 
 papers = arxiv.query("cat:astro-ph", max_results=500)
 
-fn = datetime.datetime.now().replace(" ","_")
+fn = datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%s")
  
 with open(f"data/{fn}.json", "w") as fh:
     json.dump(papers, fh)
